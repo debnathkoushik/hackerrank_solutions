a0532422e93577718e929a1904fcf508a8f7b138
debnathkoushik <debnathkoushik605@gmail.com>
2018-01-14 11:36:51 +0530

    Editted the comment

--- input_output/inputOutput.cpp
+++ input_output/inputOutput.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    /* Read input from STDIN. Print output to STDOUT */   
     return 0;
 }
 
